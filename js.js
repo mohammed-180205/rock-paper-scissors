ce684dba1d7da057808e37a0c918443efef039bf
Mohammed <mohammed.180205@gmail.com>
2026-02-21 16:04:09 +0300

    writing the logic to play a single round

--- js.js
+++ js.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice(){
     let symbol = Math.floor(Math.random() * 3);
     if(symbol === 0){
@@ -30,4 +34,59 @@ let humanChoice = prompt("type rock, paper or scissors");
 
 }
 
-getHumanChoice();
+
+
+
+
+
+function playRound(humanChoice, computerChoice){
+
+
+if(humanChoice === "rock" && computerChoice === "scissors"){
+    humanScore++;
+    return "You win! Rock beats Scissors";
+}
+
+else if(humanChoice === "rock" && computerChoice === "paper"){
+    computerScore++;
+return "You lose! Paper beats Rock";
+}
+
+else if(humanChoice === "rock" && computerChoice === "rock"){
+return "Its a tie."
+}
+
+else if(humanChoice === "paper" && computerChoice === "rock"){
+    humanScore++;
+return "You win! Paper beats Rock";
+}
+
+else if(humanChoice === "paper" && computerChoice === "scissors"){
+    computerScore++;
+return "You lose! Scissors beats Paper";
+}
+
+else if(humanChoice === "paper" && computerChoice === "paper"){
+return "Its a tie."
+}
+
+else if(humanChoice === "scissors" && computerChoice === "paper"){
+    humanScore++;
+return "You win! Scissors beats Paper";
+}
+
+else if(humanChoice === "scissors" && computerChoice === "rock"){
+    computerScore++;
+return "You lose! Rock beats Scissors";
+}
+
+else if(humanChoice === "scissors" && computerChoice === "scissors"){
+return "Its a tie."
+}
+else {
+    return "type a valid choice";
+}
+
+}
+
+console.log(playRound(getHumanChoice(), getComputerChoice()));
